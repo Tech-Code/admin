a3b3e0ee81901feb65cf655090a0040f0f6b0aef
xuefei <xuefei016@163.com>
2014-06-12 13:59:01 +0800

    修改注释

--- bus-dao/src/main/java/com/techapi/bus/BusConstants.java
+++ bus-dao/src/main/java/com/techapi/bus/BusConstants.java
@@ -11,7 +11,7 @@ public final class BusConstants {
     public static final DateTimeFormatter FORMATTER_TIME = DateTimeFormat.forPattern("yyyy-MM-dd HH:mm:ss");
     public static final DateTimeFormatter FORMATTER_TIMESTAMP = DateTimeFormat.forPattern("yyyy-MM-dd HH:mm:ss,SSS");
     /**
-     * 时速表无城市默认值
+     * 时速表无City默认值
      */
     public static final String DEFAULT_CITYNAME="默认";
 
