0733abc53bf04a1d5a964bb39a098dd019257ff4
Andrew Xue <xuefei016@163.com>
2014-06-14 16:12:09 +0800

    添加POI管理操作

--- bus-dao/src/test/java/com/techapi/bus/dao/PoiDaoTest.java
+++ bus-dao/src/test/java/com/techapi/bus/dao/PoiDaoTest.java
@@ -1,16 +1,14 @@
 package com.techapi.bus.dao;
 
-import java.util.List;
-
-import javax.annotation.Resource;
-
+import com.techapi.bus.entity.Poi;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.techapi.bus.entity.Poi;
+import javax.annotation.Resource;
+import java.util.List;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration("classpath:META-INF/applicationContext-bus-dao-oracle.xml")
@@ -28,5 +26,11 @@ public class PoiDaoTest {
 			}
 		}
 	}
+
+    @Test
+    public void testFindAll() {
+        List<Poi> list = (List<Poi>)dao.findAll();
+        System.out.println("list.size: " + list.size());
+    }
 
 }
