1f3aaedb58f95bebaf839bcff00b9754332680a3
jiayusun <jiayusun@sohu-inc.com>
2014-06-23 09:58:13 +0800

    日志分析web提交

--- bus-dao/src/main/java/com/techapi/bus/dao/AnalysisCityDao.java
+++ bus-dao/src/main/java/com/techapi/bus/dao/AnalysisCityDao.java
@@ -1,9 +1,22 @@
 package com.techapi.bus.dao;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 
 import com.techapi.bus.entity.AnalysisCity;
 
 public interface AnalysisCityDao  extends PagingAndSortingRepository<AnalysisCity, String>{
 
+	@Query("select a from AnalysisCity a where a.cityName = :name and a.day >=:startTime and a.day<=:endTime")
+	public List<AnalysisCity> findByTimeAndType(@Param("name") String name,@Param("startTime") String startTime,@Param("endTime") String endTime);
+
+	@Query("select a from AnalysisCity a where a.day >=:startTime and a.day<=:endTime")
+	public List<AnalysisCity> findByTime(@Param("startTime") String startTime,@Param("endTime") String endTime);
+	
+	@Query("select distinct a.cityName from AnalysisCity a")
+	public List<String> findTypeAll();
+	
 }
--- bus-web/src/main/java/com/techapi/bus/controller/AnalysisController.java
+++ bus-web/src/main/java/com/techapi/bus/controller/AnalysisController.java
@@ -1,23 +1,56 @@
 package com.techapi.bus.controller;
 
+import java.util.List;
 import java.util.Map;
 
 import javax.annotation.Resource;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.techapi.bus.service.AnalysisService;
+import com.techapi.bus.vo.SpringMap;
 @Controller
 @RequestMapping("/analysis")
 public class AnalysisController {
 	@Resource
 	public AnalysisService analysisService;
 	
-	@RequestMapping("/list")
+	@RequestMapping("/typelist")
     @ResponseBody
-	public Map<String, Object> list() throws Exception {
-        return null;
+	public Map<String, Object> typeList(@RequestParam(value="type",required = false) String type,@RequestParam(value="startTime",required = false) String startTime,@RequestParam(value="endTime",required = false) String endTime) throws Exception {
+		return analysisService.findAnalysisTypeByTimeAndType(type, startTime, endTime);
+	}
+	
+	@RequestMapping("/type")
+    @ResponseBody
+	public List<SpringMap> type() throws Exception {
+		return analysisService.findTypeAll();
+	}
+	
+	@RequestMapping("/citylist")
+    @ResponseBody
+	public Map<String, Object> cityList(@RequestParam(value="city",required = false) String city,@RequestParam(value="startTime",required = false) String startTime,@RequestParam(value="endTime",required = false) String endTime) throws Exception {
+		return analysisService.findAnalysisCityByTimeAndName(city, startTime, endTime);
+	}
+	
+	@RequestMapping("/city")
+    @ResponseBody
+	public List<SpringMap> city() throws Exception {
+		return analysisService.findCityAll();
+	}
+	
+	@RequestMapping("/namelist")
+    @ResponseBody
+	public Map<String, Object> nameList(@RequestParam(value="name",required = false) String name,@RequestParam(value="startTime",required = false) String startTime,@RequestParam(value="endTime",required = false) String endTime) throws Exception {
+		return analysisService.findAnalysisGroupByTimeAndType(name, startTime, endTime);
+	}
+	
+	@RequestMapping("/name")
+    @ResponseBody
+	public List<SpringMap> name() throws Exception {
+		return analysisService.findNameAll();
 	}
 }
